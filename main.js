ceca2d28bd8707e6dcf156b50258af465e08c447
peizhi <dfpeizhi@163.com>
2019-12-22 23:22:09 +0800

    设置窗口

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-const { app, BrowserWindow, Menu } = require('electron')
-const isDev = require('electron-is-dev')
-const menuTemplate = require('./src/menuTemplate')
-let mainWindow
-app.on('ready', () => {
-  mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 1200,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
-  const urlLocation = isDev ? "http://localhost:3000" : "FFFFFF"
-  mainWindow.loadURL(urlLocation)
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+const menuTemplate = require("./src/menuTemplate");
+const AppWindow = require("./src/AppWindow");
+let mainWindow, settingsWindow;
+app.on("ready", () => {
+  const mainWindowConfig = {
+    width: 1440,
+    height: 768
+  };
+  const urlLocation = isDev ? "http://localhost:3000" : "FFFFFF";
+  mainWindow = new AppWindow(mainWindowConfig, urlLocation);
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 
-  const menu = Menu.buildFromTemplate(menuTemplate)
-  Menu.setApplicationMenu(menu)
-})
+  ipcMain.on("open-settings-window", () => {
+    const settingsWindowConfig = {
+      width: 500,
+      height: 400,
+      parent: mainWindow
+    };
+    const settingsFileLocation = `file://${path.join(
+      __dirname,
+      "./settings/settings.html"
+    )}`;
+    settingsWindow = new AppWindow(settingsWindowConfig, settingsFileLocation);
+    settingsWindow.on("closed", () => {
+      settingsWindow = null;
+    });
+  });
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
+});
--- src/App.js
+++ src/App.js
@@ -1,185 +1,192 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import FileSearch from './component/FileSearch'
-import FileList from './component/FileList'
-import BottomBtn from './component/BottomBtn'
-import { faPlus, faFileImport, faSave } from '@fortawesome/free-solid-svg-icons'
-import TabList from './component/TabList'
-import SimpleMDE from "react-simplemde-editor"
-import "easymde/dist/easymde.min.css"
-import uuidv4 from 'uuid/v4'
-import { flattenArr, objToArr } from './utils/helper'
-import fileHelper from './utils/fileHelper'
-import useIpcRenderer from './hooks/useIpcRenderer'
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import FileSearch from "./component/FileSearch";
+import FileList from "./component/FileList";
+import BottomBtn from "./component/BottomBtn";
+import {
+  faPlus,
+  faFileImport,
+  faSave
+} from "@fortawesome/free-solid-svg-icons";
+import TabList from "./component/TabList";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import uuidv4 from "uuid/v4";
+import { flattenArr, objToArr } from "./utils/helper";
+import fileHelper from "./utils/fileHelper";
+import useIpcRenderer from "./hooks/useIpcRenderer";
 
-const { join, basename, extname, dirname } = window.require('path')
-const { remote, ipcRenderer } = window.require('electron')
-const Store = window.require('electron-store')
-const fileStore = new Store({ 'name': 'Files Data' })
+const { join, basename, extname, dirname } = window.require("path");
+const { remote, ipcRenderer } = window.require("electron");
+const Store = window.require("electron-store");
+const fileStore = new Store({ name: "Files Data" });
+const settingsStore = new Store({ name: "Settings" });
 
-const saveFilesToStore = (files) => {
+const saveFilesToStore = files => {
   const filesStoreObj = objToArr(files).reduce((result, file) => {
-    const { id, path, title, createdAt } = file
+    const { id, path, title, createdAt } = file;
     result[id] = {
       id,
       path,
       title,
-      createdAt,
-    }
-    return result
-  }, {})
-  fileStore.set('files', filesStoreObj)
-}
-function App () {
-  const [files, setFiles] = useState(fileStore.get('files') || {})
-  const [activeFileID, setActiveFileID] = useState('')
-  const [openedFileIDs, setOpenedFileIDs] = useState([])
-  const [unsavedFileIDs, setUnsavedFileIDs] = useState([])
-  const [searchedFiles, setSearchedFiles] = useState([])
-  const filesArr = objToArr(files)
-  const savedLocation = remote.app.getPath('documents')
+      createdAt
+    };
+    return result;
+  }, {});
+  fileStore.set("files", filesStoreObj);
+};
+function App() {
+  const [files, setFiles] = useState(fileStore.get("files") || {});
+  const [activeFileID, setActiveFileID] = useState("");
+  const [openedFileIDs, setOpenedFileIDs] = useState([]);
+  const [unsavedFileIDs, setUnsavedFileIDs] = useState([]);
+  const [searchedFiles, setSearchedFiles] = useState([]);
+  const filesArr = objToArr(files);
+  const savedLocation =
+    settingsStore.get("fileLocation") || remote.app.getPath("documents");
 
   const openedFiles = openedFileIDs.map(openID => {
-    return files[openID]
-  })
-  const fileListArr = (searchedFiles.length > 0) ? searchedFiles : filesArr
-  const activeFile = files[activeFileID]
-  const fileClick = (fileID) => {
-    setActiveFileID(fileID)
-    const currentFile = files[fileID]
+    return files[openID];
+  });
+  const fileListArr = searchedFiles.length > 0 ? searchedFiles : filesArr;
+  const activeFile = files[activeFileID];
+  const fileClick = fileID => {
+    setActiveFileID(fileID);
+    const currentFile = files[fileID];
     if (!currentFile.isLoaded) {
       fileHelper.readFile(currentFile.path).then(value => {
-        const newFile = { ...files[fileID], body: value, isLoaded: true }
-        setFiles({ ...files, [fileID]: newFile })
-      })
+        const newFile = { ...files[fileID], body: value, isLoaded: true };
+        setFiles({ ...files, [fileID]: newFile });
+      });
     }
     if (!openedFileIDs.includes(fileID)) {
-      setOpenedFileIDs([...openedFileIDs, fileID])
+      setOpenedFileIDs([...openedFileIDs, fileID]);
     }
-  }
-  const tabClick = (fileID) => {
-    setActiveFileID(fileID)
-  }
-  const tabClose = (id) => {
-    const tabWithout = openedFileIDs.filter(fileID => fileID !== id)
-    setOpenedFileIDs(tabWithout)
+  };
+  const tabClick = fileID => {
+    setActiveFileID(fileID);
+  };
+  const tabClose = id => {
+    const tabWithout = openedFileIDs.filter(fileID => fileID !== id);
+    setOpenedFileIDs(tabWithout);
     if (tabWithout.length > 0) {
-      setActiveFileID(tabWithout[0])
+      setActiveFileID(tabWithout[0]);
     } else {
-      setActiveFileID('')
+      setActiveFileID("");
     }
-  }
+  };
   const fileChange = (id, value) => {
     if (value !== files[id].body) {
-      const newFile = { ...files[id], body: value }
-      setFiles({ ...files, [id]: newFile })
+      const newFile = { ...files[id], body: value };
+      setFiles({ ...files, [id]: newFile });
       if (!unsavedFileIDs.includes(id)) {
-        setUnsavedFileIDs([...unsavedFileIDs, id])
+        setUnsavedFileIDs([...unsavedFileIDs, id]);
       }
     }
-  }
-  const deleteFile = (id) => {
+  };
+  const deleteFile = id => {
     if (files[id].isNew) {
-      const { [id]: value, ...afterDelete } = files
-      setFiles(afterDelete)
+      const { [id]: value, ...afterDelete } = files;
+      setFiles(afterDelete);
     } else {
       fileHelper.deleteFile(files[id].path).then(() => {
-        const { [id]: value, ...afterDelete } = files
-        setFiles(afterDelete)
-        saveFilesToStore(afterDelete)
-        tabClose(id)
-      })
+        const { [id]: value, ...afterDelete } = files;
+        setFiles(afterDelete);
+        saveFilesToStore(afterDelete);
+        tabClose(id);
+      });
     }
-  }
+  };
   const updateFileName = (id, title, isNew) => {
-    const newPath = isNew ? join(savedLocation, `${title}.md`) : join(dirname(files[id].path), `${title}.md`)
-    const modifiedFile = { ...files[id], title, isNew: false, path: newPath }
-    const newFiles = { ...files, [id]: modifiedFile }
+    const newPath = isNew
+      ? join(savedLocation, `${title}.md`)
+      : join(dirname(files[id].path), `${title}.md`);
+    const modifiedFile = { ...files[id], title, isNew: false, path: newPath };
+    const newFiles = { ...files, [id]: modifiedFile };
     if (isNew) {
       fileHelper.writeFile(newPath, files[id].body).then(() => {
-        setFiles(newFiles)
-        saveFilesToStore(newFiles)
-      })
+        setFiles(newFiles);
+        saveFilesToStore(newFiles);
+      });
     } else {
-      const oldPath = files[id].path
+      const oldPath = files[id].path;
       fileHelper.renameFile(oldPath, newPath).then(() => {
-        setFiles(newFiles)
-        saveFilesToStore(newFiles)
-      })
+        setFiles(newFiles);
+        saveFilesToStore(newFiles);
+      });
     }
-  }
-  const fileSearch = (keyword) => {
-    const newFiles = filesArr.filter(file => file.title.includes(keyword))
-    setSearchedFiles(newFiles)
-  }
+  };
+  const fileSearch = keyword => {
+    const newFiles = filesArr.filter(file => file.title.includes(keyword));
+    setSearchedFiles(newFiles);
+  };
   const createNewFile = () => {
-    const newID = uuidv4()
+    const newID = uuidv4();
     const newFile = {
       id: newID,
-      title: '',
-      body: '##请输入内容',
+      title: "",
+      body: "##请输入内容",
       createAt: new Date().getTime(),
-      isNew: true,
-    }
-    setFiles({ ...files, [newID]: newFile })
-  }
+      isNew: true
+    };
+    setFiles({ ...files, [newID]: newFile });
+  };
   const saveCurrentFile = () => {
     if (activeFile) {
       fileHelper.writeFile(activeFile.path, activeFile.body).then(() => {
-        setUnsavedFileIDs(unsavedFileIDs.filter(id => id !== activeFile.id))
-      })
+        setUnsavedFileIDs(unsavedFileIDs.filter(id => id !== activeFile.id));
+      });
     }
-  }
+  };
   const importFiles = () => {
-    remote.dialog.showOpenDialog({
-      title: '选择导入的文件',
-      properties: ['openFile', 'multiSelections'],
-      filters: [
-        { name: 'Markdown files', extensions: ['md'] }
-      ]
-    }, (paths) => {
-      if (Array.isArray(paths)) {
-        const filteredPaths = paths.filter(path => {
-          const alreadyAdded = Object.values(files).find(file => {
-            return file.path === path
-          })
-          return !alreadyAdded
-        })
-        const importFilesArr = filteredPaths.map(path => {
-          return {
-            id: uuidv4(),
-            title: basename(path, extname(path)),
-            path,
+    remote.dialog.showOpenDialog(
+      {
+        title: "选择导入的文件",
+        properties: ["openFile", "multiSelections"],
+        filters: [{ name: "Markdown files", extensions: ["md"] }]
+      },
+      paths => {
+        if (Array.isArray(paths)) {
+          const filteredPaths = paths.filter(path => {
+            const alreadyAdded = Object.values(files).find(file => {
+              return file.path === path;
+            });
+            return !alreadyAdded;
+          });
+          const importFilesArr = filteredPaths.map(path => {
+            return {
+              id: uuidv4(),
+              title: basename(path, extname(path)),
+              path
+            };
+          });
+          // console.log(importFilesArr)
+          const newFiles = { ...files, ...flattenArr(importFilesArr) };
+          // console.log(newFiles)
+          setFiles(newFiles);
+          saveFilesToStore(newFiles);
+          if (importFilesArr.length > 0) {
+            remote.dialog.showMessageBox({
+              type: "info",
+              title: `成功导入了${importFilesArr.length}个文件`,
+              message: `成功导入了${importFilesArr.length}个文件`
+            });
           }
-        })
-        // console.log(importFilesArr)
-        const newFiles = { ...files, ...flattenArr(importFilesArr) }
-        // console.log(newFiles)
-        setFiles(newFiles)
-        saveFilesToStore(newFiles)
-        if (importFilesArr.length > 0) {
-          remote.dialog.showMessageBox({
-            type: 'info',
-            title: `成功导入了${importFilesArr.length}个文件`,
-            message: `成功导入了${importFilesArr.length}个文件`,
-          })
         }
       }
-    })
-  }
+    );
+  };
   useIpcRenderer({
-    'create-new-file': createNewFile,
-    'import-file': importFiles,
-    'save-edit-file': saveCurrentFile,
-  })
+    "create-new-file": createNewFile,
+    "import-file": importFiles,
+    "save-edit-file": saveCurrentFile
+  });
   return (
     <div className="App container-fluid px-0">
       <div className="row no-gutters">
         <div className="col-3 bg-light left-panel">
-          <FileSearch
-            onFileSearch={fileSearch}
-          />
+          <FileSearch onFileSearch={fileSearch} />
           <FileList
             files={fileListArr}
             onFileClick={fileClick}
@@ -206,12 +213,8 @@ function App () {
           </div>
         </div>
         <div className="col-9 right-panel">
-          {!activeFile &&
-            <div className="start-page">
-              选择或创建新文档
-            </div>
-          }
-          {activeFile &&
+          {!activeFile && <div className="start-page">选择或创建新文档</div>}
+          {activeFile && (
             <>
               <TabList
                 files={openedFiles}
@@ -223,16 +226,18 @@ function App () {
               <SimpleMDE
                 key={activeFile && activeFile.id}
                 value={activeFile && activeFile.body}
-                onChange={(value) => { fileChange(activeFile.id, value) }}
+                onChange={value => {
+                  fileChange(activeFile.id, value);
+                }}
                 options={{
-                  minHeight: '515px',
+                  minHeight: "515px"
                 }}
               />
             </>
-          }
+          )}
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
